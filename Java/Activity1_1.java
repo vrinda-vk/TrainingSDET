2228f37ee590d500b8b08625c34478f3c93cd788
vrinda-vk <73576047+vrinda-vk@users.noreply.github.com>
2020-11-28 09:14:28 +0530

    Add files via upload

---
+++ Java/Activity1_1.java
@@ -0,0 +1,17 @@
+package automationFramework;
+
+public class Activity1_1 {
+
+    public static void main(String[] args) {
+        Car toyota = new Car();
+        toyota.make = 2014;
+        toyota.color = "Black";
+        toyota.transmission = "Manual";
+    
+        //Using Car class method
+        toyota.displayCharacterstics();
+        toyota.accelerate();
+        toyota.brake();
+    }
+
+}
